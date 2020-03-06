fb0e0d1f95eb28b5feb63ddafcfc245f9cea6032
Kushanrawindu <kushanrawindu@gmail.com>
2020-03-06 23:27:33 +0530

    signin and signup path created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          SignIn
+        </Route>
+        <Route exact path="/sign-up">
+          SignUp
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
