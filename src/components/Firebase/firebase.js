bf67645561501d00be3a9010dd5cec2a227e2fb1
Kushanrawindu <kushanrawindu@gmail.com>
2020-03-07 22:08:55 +0530

    add firebase to the project

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBkiPQ9gh5nuDsfm_E4_B8Ymgk5DJ08J9E",
+    authDomain: "activity-tracker-514c2.firebaseapp.com",
+    databaseURL: "https://activity-tracker-514c2.firebaseio.com",
+    projectId: "activity-tracker-514c2",
+    storageBucket: "activity-tracker-514c2.appspot.com",
+    messagingSenderId: "246943162387",
+    appId: "1:246943162387:web:fd8866ee006411e44b9fe6",
+    measurementId: "G-DYX1547ZCP"
+};
+
+class Firebase {
+    constructor(){
+        app.initializeApp(config);
+        this.auth = app.auth();
+        //this.db = app.database();
+
+        /*** Authentication  ***/
+        doCreateUserWithEmailAndPassword = (email, password) => 
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+        doSignInWithEmailAndPassword = (email, password) => 
+        this.auth.signInWithEmailAndPassword(email, password);
+
+        doSignOut = () => 
+        this.auth.signOut();
+
+        doPasswordReset = email => 
+        this.auth.sendPasswordResetEmail(email);
+    }
+}
+
+export default Firebase;
